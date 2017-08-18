e81d65b325656397ac05143e5348b6427003d8c9
yaoyichi2011 <yaoyichi2011@users.noreply.github.com>
2017-08-18 22:31:30 +0800

    Delete cinstall.php

--- lib/cinstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!file_exists('./install/install.lock')){
-    echo '<meta charset="utf-8"><title>跳转中</title><meta http-equiv="Refresh" content="1;url=./install">您未安装本程序,正在跳转至安装页面';
-    die;
-}
