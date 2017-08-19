b5c97eb718483bc006662a0bde2a04ca321f1ab2
yaoyichi2011 <yaoyichi2011@users.noreply.github.com>
2017-08-19 13:06:22 +0800

    5.3.2
    修复若干bug，新添若干bug（〃｀ 3′〃）

---
+++ list.php
@@ -0,0 +1,74 @@
+<?php
+require './config.php';
+ignore_user_abort(true);
+function  showlistname ($path){
+	$listname=strtok($path,"/");
+	$a='[';
+	while ($listname!==false){
+		$a.="\"{$listname}\",";
+		$listname=strtok("/");
+	}
+	$a.="\"\"]";
+	$b=json_decode($a,1);
+	$aaa='';
+	for ($x=0;
+	$x<=count($b)-2;
+	$x++){
+		$aaa.= '<li><a href="./?m=list&order='.$order.'&by='.$by.'&page=1&path=';
+		for ($y=0;
+		$y<=$x;
+		$y++){
+			$aaa.= $b[$y].'/';
+		}
+		$aaa.= '">'.$b[$x].'</a></li>';
+	}
+	return $aaa;
+}
+$list=$_GET['page'];
+$bduss=urlencode($_COOKIE['bduss']);
+$order=$_GET['order'];
+$by=$_GET['by'];
+if (strlen($_GET['path'])<=0){
+	$path='%2F';
+}else {
+	$path=$_GET['path'];
+}
+if (strlen($list)<=0){
+    echo '<meta http-equiv="Refresh" content="0;url=./?m=list&order='.$order.'&by='.$by.'&page=1&path=/">';
+	$url='http://pcs.baidu.com/rest/2.0/pcs/file?path=/'.$path.'&method=list&app_id='.$appid.'&by=name&by='.$by.'&order='.$order.'&limit=0-10';
+}
+elseif ($list<=0){
+	echo '<meta http-equiv="Refresh" content="0;url=./?m=list&order='.$order.'&by='.$by.'&page=1&path=/"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">参数非法</p></div></div></div><br/>';
+}else {
+	$url='http://pcs.baidu.com/rest/2.0/pcs/file?path=/'.$path.'&method=list&app_id='.$appid.'&by=name&by='.$by.'&order='.$order.'&limit='.($list-1).'0-'.$list.'0';
+}
+$ch=curl_init($url);
+curl_setopt($ch,CURLOPT_USERAGENT ,'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER ,1);
+curl_setopt($ch,CURLOPT_COOKIE ,'BDUSS='.$bduss);
+curl_setopt($ch,CURLOPT_REFERER ,'pcs.baidu.com');
+$content=curl_exec($ch);
+curl_close($ch);
+if (json_decode($content,1)["error_code"]==31045){
+	echo '<meta http-equiv="Refresh" content="2;url=./"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">您还没有登录或文件不存在</p></div></div></div>';
+}else {
+	$b=json_decode($content,1);
+	echo '<div class="col-md-12" role="main"><ol class="breadcrumb"><li><a href="./?m=list&order='.$order.'&by='.$by.'&page='.$list.'&path=/">根目录</a></li>'.showlistname(urldecode($path)).'</ol><center><div class="table-responsive"><table  class="table table-hover table-striped table-condensed table-responsive" ><th class="mdl-data-table__cell--non-numeric"  style="width:5%">类型</th><th class="mdl-data-table__cell--non-numeric ">文件名称</th><th class="mdl-data-table__cell--non-numeric"  style="width:9%">下载1</th><th class="mdl-data-table__cell--non-numeric"  style="width:9%">下载2</th>';
+	for ($x=0;
+	$x<=count($b["list"])-1;
+	$x++){
+		if ($b["list"][$x]["isdir"]==1){
+			echo '<tr><td class="mdl-data-table__cell--non-numeric">文件夹</td><td class="mdl-data-table__cell--non-numeric"><a href="./?m=list&by='.$by.'&order='.$order.'&path='.urlencode($b["list"][$x]["path"]).'&page=1">'.$b["list"][$x]["server_filename"].'</a></td><td class="mdl-data-table__cell--non-numeric"></td><td class="mdl-data-table__cell--non-numeric"></td></tr>';
+		}else {
+			echo '<tr><td class="mdl-data-table__cell--non-numeric">文件</td><td class="mdl-data-table__cell--non-numeric">'.$b["list"][$x]["server_filename"].'</td><td class="mdl-data-table__cell--non-numeric"><a href="./?m=car&path='.urlencode($b["list"][$x]["path"]).'"target="_blank">获取高速连接1</a></td><td class="mdl-data-table__cell--non-numeric"><a href="./?m=suv&path='.urlencode($b["list"][$x]["path"]).'"target="_blank">获取高速链接2</a></td></tr>';
+		}
+	}
+}
+echo '</table></div></center></div><br><br><nav aria-label="..."><ul class="pager">';
+if ($list==!1){
+	echo '<li class="previous"><a href="./?m=list&by='.$by.'&order='.$order.'&path='.urlencode($path).'&page='.($list-1).'"><span aria-hidden="true">&larr;</span> 上一页 </a></li>';
+}
+if (count($b["list"])>=10){
+	echo '<li class="next"><a href="./?m=list&by='.$by.'&order='.$order.'&path='.urlencode($path).'&page='.($list+1).'"> 下一页 <span aria-hidden="true">&rarr;</span></a></li></ul></nav>';
+}
+echo '</div>';
---
+++ main.php
@@ -0,0 +1,51 @@
+<?php
+require './config.php';
+require './lib/head.php';
+if ($_GET["posturl"]=='suv'){
+	$posturl='./?m=suv';
+}else {
+	$posturl='./?m=car';
+}
+if (strlen($_COOKIE['bduss'])>0){
+	$check_login=json_decode(scurl ('http://tieba.baidu.com/dc/common/tbs','get','','BDUSS='.$_COOKIE['bduss'],'','','',''),1)["is_login"];
+	if ($check_login==1){
+		echo '<div class="col-md-8 col-md-offset-2 " role="main"><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">说明</h3></div><div class="panel-body"><span id="avatar" style="float:right;"><img id="iavatar" src="./?m=avatar" class="img-rounded" height=\'55px\' width=\'55px\' "></span>';
+		if ($posturl=='./?m=suv'){
+			echo '若需要用第一种获取方式请点<a href="./" >这里</a>';
+		}else {
+			echo '若需要用第二种获取方式请点<a href="./?posturl=suv" >这里</a>';
+		}
+		echo '<br><i>*填写绝对路径</i><br>*绝对路径:如在首页名为"x"的文件夹内名称为"k.png"的文件请填写"x/k.png"<!--<br /><a href="./old?fr=new">旧版入口</a>--></div></div><center><form action="'.$posturl.'" method="post"><div class="input-group"><span class="input-group-addon" id="basic-addon3">/</span><input type="text" placeholder="文件路径..." class="form-control" name="path" aria-describedby="sizing-addon1"><span class="input-group-btn"><button class="btn btn-default" type="submit">点击获取直链</button></span></div></div></form></center>';
+	}else {
+		echo '<title>跳转中</title><meta http-equiv="Refresh" content="1;url=./logout.php"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">bduss无效...</p></div></div></div>';
+	}
+}
+elseif (strlen($_GET['bduss'])>0){
+	if ($chinamode==0 && $secret!==''){
+		$captchaback=json_decode(scurl ('https://www.google.com/recaptcha/api/siteverify','post','secret='.$secret.'&response='.$_GET["g-recaptcha-response"].'&remoteip='.$_SERVER["HTTP_X_FORWARDED_FOR"],'','',3,'',''),1);
+		if ($captchaback["success"]!=1){
+			echo '<title>跳转中</title><meta http-equiv="Refresh" content="5;url='.$_SERVER['HTTP_REFERER'].'"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">reCAPTCHA验证失败,请重试...</p></div></div></div>';
+			die;
+		}
+	}
+	$check_login=json_decode(scurl ('http://tieba.baidu.com/dc/common/tbs','get','','BDUSS='.$_GET['bduss'],'',1,'',''),1)["is_login"];
+	if ($check_login==1){
+		if ($_GET["rm"]==1){
+			setcookie('bduss',$_GET['bduss'],time()+315705600,'/',$_SERVER['HTTP_HOST']);
+			setcookie('ptoken',$_GET['ptoken'],time()+315705600,'/',$_SERVER['HTTP_HOST']);
+			setcookie('stoken',$_GET['stoken'],time()+315705600,'/',$_SERVER['HTTP_HOST']);
+			setcookie('baiduid',json_decode(head ($_GET['bduss']),1)["un"],time()+315705600,'/',$_SERVER['HTTP_HOST']);
+			echo '<title>跳转中</title><meta http-equiv="Refresh" content="0;url=./"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">跳转中...</p></div></div></div>';
+		}else {
+			setcookie('bduss',$_GET['bduss'],time()+300,'/',$_SERVER['HTTP_HOST']);
+			setcookie('ptoken',$_GET['ptoken'],time()+300,'/',$_SERVER['HTTP_HOST']);
+			setcookie('stoken',$_GET['stoken'],time()+300,'/',$_SERVER['HTTP_HOST']);
+			setcookie('baiduid',json_decode(head ($_GET['bduss']),1)["un"],time()+300,'/',$_SERVER['HTTP_HOST']);
+			echo '<title>跳转中</title><meta http-equiv="Refresh" content="2;url=./"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">未设置为保存cookie，cookie生命周期设置为300s，跳转中...</p></div></div></div>';
+		}
+	}else {
+		echo '<title>跳转中</title><meta http-equiv="Refresh" content="1;url=./"><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-default"><div class="panel-body"><p class="text-center">bduss无效...</p></div></div></div>';
+	}
+}else {
+	include ('./lib/bus.php');
+}
