dd9071953be7717ca4a78a5261dd663d4fd06d4a
TREEMAN <yaoyichi2011@users.noreply.github.com>
2017-10-02 14:43:21 +0800

    v5.6
    fix some bug

--- lib/bus.php
+++ lib/bus.php
@@ -1,6 +1,7 @@
 <?php
+//有哪位大神能解释一下那个logid是啥玩意。。。
 require './config.php';
-echo '<br><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">指南</h3></div><div class="panel-body">请先输入链接地址(暂不支持文件夹以及文件夹内文件、加密分享、部分https链接)，点击上面的<b>提交按钮</b>后获得验证码（若无验证码请重新获取）</div></div><center>';
+echo '<br><div class="col-md-8 col-md-offset-2" role="main"><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">指南</h3></div><div class="panel-body">请先输入链接地址(暂不支持文件夹以及文件夹内文件、加密分享、部分https链接)，点击上面的<b>提交按钮</b>后获得验证码（若无验证码请重新获取），若最后获取到空白请检查验证码是否输入错误或连接是否失效并回去重新获取</div></div><center>';
 if (strlen($_POST['url'])>0){
 	if (strlen($_POST["spwd"])>0){
 		$pwurl=$_POST['url'];
@@ -20,7 +21,7 @@ if (strlen($_POST['url'])>0){
 			$pwurll='http://pan.baidu.com/wap/init?'.trim($actbc[2]);
 		}
 		//echo $actbc[2];
-		$post_pwd=curl_init('https://pan.baidu.com/share/verify?'.trim($actbc[2]).'&bdstoken=null&web=5&app_id=250528&logid=MTQ4NzA2NzE5MDU0NzAuMDg2MzE0ODYwNzMxMzYzMw==&channel=chunlei&clienttype=5');
+		$post_pwd=curl_init('https://pan.baidu.com/share/verify?'.trim($actbc[2]).'&bdstoken=null&web=5&app_id=250528&logid='.$logid.'&channel=chunlei&clienttype=5');
 		curl_setopt($post_pwd,CURLOPT_USERAGENT ,'Mozilla/5.0 (Symbian/3; Series60/5.2 NokiaN8-00/012.002; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/7.3.0 Mobile Safari/533.4 3gpp-gba');
 		curl_setopt($post_pwd,CURLOPT_RETURNTRANSFER ,1);
 		curl_setopt($post_pwd,CURLOPT_POST ,1);
@@ -49,7 +50,7 @@ if (strlen($_POST['url'])>0){
 		echo "您输入的网址:<a href=\"{$url}\">{$url}</a>";
 		$ch=curl_init($url);
 		curl_setopt($ch,CURLOPT_USERAGENT ,'Mozilla/5.0 (Symbian/3; Series60/5.2 NokiaN8-00/012.002; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/7.3.0 Mobile Safari/533.4 3gpp-gba');
-                curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+		curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER ,1);
 		curl_setopt($ch,CURLOPT_HEADER ,1);
 		$content=curl_exec($ch);
@@ -58,7 +59,7 @@ if (strlen($_POST['url'])>0){
 	//echo $content;
 	preg_match_all('/window.yunData = (.+?);/iu',$content,$kd);
 	$json=json_decode($kd[1][0],1);
-	$zh=curl_init("http://pan.baidu.com/share/download?bdstoken=null&web=5&app_id=250528&logid=MTQ4NzA2NzE5MDU0NzAuMDg2MzE0ODYwNzMxMzYzMw==&channel=chunlei&clienttype=5&uk={$json["uk"]}&shareid={$json["shareid"]}&fid_list=%5B{$json["file_list"][0]["fs_id"]}%5D&sign={$json["downloadsign"]}&timestamp={$json["timestamp"]}");
+	$zh=curl_init("http://pan.baidu.com/share/download?bdstoken=null&web=5&app_id=250528&logid={$logid}&channel=chunlei&clienttype=5&uk={$json["uk"]}&shareid={$json["shareid"]}&fid_list=%5B{$json["file_list"][0]["fs_id"]}%5D&sign={$json["downloadsign"]}&timestamp={$json["timestamp"]}");
 	curl_setopt($zh,CURLOPT_USERAGENT ,'Mozilla/5.0 (Symbian/3; Series60/5.2 NokiaN8-00/012.002; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/7.3.0 Mobile Safari/533.4 3gpp-gba');
 	curl_setopt($zh,CURLOPT_RETURNTRANSFER ,1);
 	$furljson=curl_exec($zh);
@@ -84,7 +85,7 @@ elseif (strlen($_GET['vcode'])>0){
 	$timestamp=$_GET["timestamp"];
 	$vcode=$_GET["vcode"];
 	$input=$_POST["input"];
-	$url="http://pan.baidu.com/share/download?bdstoken=null&web=5&app_id=250528&logid=MTQ4NzA2NzE5MDU0NzAuMDg2MzE0ODYwNzMxMzYzMw==&channel=chunlei&clienttype=5&uk={$uk}&shareid={$shareid}&fid_list={$fid_list}&sign={$downloadsign}&timestamp={$timestamp}&input={$input}&vcode={$vcode}";
+	$url="http://pan.baidu.com/share/download?bdstoken=null&web=5&app_id=250528&logid={$logid}&channel=chunlei&clienttype=5&uk={$uk}&shareid={$shareid}&fid_list={$fid_list}&sign={$downloadsign}&timestamp={$timestamp}&input={$input}&vcode={$vcode}";
 	$ch=curl_init($url);
 	curl_setopt($ch,CURLOPT_USERAGENT ,'Mozilla/5.0 (Symbian/3; Series60/5.2 NokiaN8-00/012.002; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/7.3.0 Mobile Safari/533.4 3gpp-gba');
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER ,1);
@@ -107,8 +108,7 @@ elseif (strlen($_GET['vcode'])>0){
 	elseif ($_GET['type']==video ){
 		echo '<video src="'.$json[1][0].'" controls="controls"></video>';
 	}else {
-		$shref=file_get_contents($siteurl.'/s.php?type=1&url='.base64_encode($json[1][0]));
-		echo "<div class=\"col-md-12\" role=\"main\"><div class=\"panel panel-default\"><div class=\"panel-body\">下载地址(如果空白请检查验证码是否输入错误或连接是否失效并回去重新获取):<a href=\"{$json[1][0]}\"\"target=\"_blank\">{$json[1][0]}</a><br><img src=\"{$shref}\"></div></div></div><br>";
+		echo "<div class=\"col-md-12\" role=\"main\"><div class=\"panel panel-default\"><div class=\"panel-body\">下载地址1:<a href=\"{$json[1][0]}\"\"target=\"_blank\">{$json[1][0]}</a><br></div></div></div><div class=\"col-md-12\" role=\"main\"><div class=\"panel panel-default\"><div class=\"panel-body\">下载地址2:<a href=\"{$link["dlink"]}\"\"target=\"_blank\">{$link["dlink"]}</a><br></div></div></div><br>";
 	}
 }else {
 	echo '<center><form action="./?m=bus" method="post"><div class="input-group"><input type="url" placeholder="请输入分享链接..." class="form-control" name="url" aria-describedby="sizing-addon1"><span class="input-group-btn"><button class="btn btn-default" type="submit">提交</button></span></div></div></form></center>';
